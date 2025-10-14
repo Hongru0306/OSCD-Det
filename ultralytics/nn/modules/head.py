ba7617658561376140a381fc6846e787d2ec051d
Oneb1 <99795077+Hongru0306@users.noreply.github.com>
2025-10-14 17:06:26 +0800

    Update codes

--- ultralytics/nn/modules/head.py
+++ ultralytics/nn/modules/head.py
@@ -15,6 +15,10 @@ from .block import DFL, BNContrastiveHead, ContrastiveHead, Proto
 from .conv import Conv, DWConv, DSConv
 from .transformer import MLP, DeformableTransformerDecoder, DeformableTransformerDecoderLayer
 from .utils import bias_init_with_prob, linear_init
+from ..modules.conv import autopad
+from mmcv.ops import ModulatedDeformConv2d
+from mmcv.cnn import build_activation_layer, build_norm_layer
+import torch.nn.functional as F
 
 __all__ = "Detect", "Segment", "Pose", "Classify", "OBB", "RTDETRDecoder", "v10Detect", "I2P"
 
@@ -607,30 +611,239 @@ class v10Detect(Detect):
 
 
 
-# import torch.nn.functional as F
+def gcd(a, b):
+    """Calculate the greatest common divisor (GCD) of two integers."""
+    return math.gcd(a, b)
+
+class Conv_GN(nn.Module):
+
+    default_act = nn.SiLU()  # default activation
+
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True, use_gcd_group=False):
+        """Initialize Conv layer with given arguments including activation."""
+        super().__init__()
+        if use_gcd_group:
+            g = gcd(c1, c2)
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
+        self.gn = nn.GroupNorm(16, c2)
+        self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+
+    def forward(self, x):
+        """Apply convolution, batch normalization and activation to input tensor."""
+        return self.act(self.gn(self.conv(x)))
+
+class TaskDecomposition(nn.Module):
+    def __init__(self, feat_channels, stacked_convs, la_down_rate=8):
+        super(TaskDecomposition, self).__init__()
+        self.feat_channels = feat_channels
+        self.stacked_convs = stacked_convs
+        self.in_channels = self.feat_channels * self.stacked_convs
+        self.la_conv1 = nn.Conv2d( self.in_channels,  self.in_channels // la_down_rate, 1)
+        self.relu = nn.ReLU(inplace=True)
+        self.la_conv2 = nn.Conv2d( self.in_channels // la_down_rate,  self.stacked_convs, 1, padding=0)
+        self.sigmoid = nn.Sigmoid()
+        
+        self.reduction_conv = Conv_GN(self.in_channels, self.feat_channels, 1)
+        self.init_weights()
+        
+    def init_weights(self):
+
+        
+        torch.nn.init.normal_(self.la_conv1.weight.data, mean=0, std=0.001)
+        torch.nn.init.normal_(self.la_conv2.weight.data, mean=0, std=0.001)
+        torch.nn.init.zeros_(self.la_conv2.bias.data)
+        torch.nn.init.normal_(self.reduction_conv.conv.weight.data, mean=0, std=0.01)
+
+    def forward(self, feat, avg_feat=None):
+        b, c, h, w = feat.shape
+        if avg_feat is None:
+            avg_feat = F.adaptive_avg_pool2d(feat, (1, 1))
+        weight = self.relu(self.la_conv1(avg_feat))
+        weight = self.sigmoid(self.la_conv2(weight))
+
+        conv_weight = weight.reshape(b, 1, self.stacked_convs, 1) * \
+                          self.reduction_conv.conv.weight.reshape(1, self.feat_channels, self.stacked_convs, self.feat_channels)
+        conv_weight = conv_weight.reshape(b, self.feat_channels, self.in_channels)
+        feat = feat.reshape(b, self.in_channels, h * w)
+        feat = torch.bmm(conv_weight, feat).reshape(b, self.feat_channels, h, w)
+        feat = self.reduction_conv.gn(feat)
+        feat = self.reduction_conv.act(feat)
+
+        return feat
 
 
-class I2P(OBB):
-    # Relevant source will be released upon paper acceptance
+
+
+class Conv_DPGN(nn.Module):
+
+    default_act = nn.SiLU()  # default activation
+
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
+        super().__init__()
+
+        self.depthwise = nn.Conv2d(c1, c1, k, s, autopad(k, p, d), groups=c1, dilation=d, bias=False)
+        self.pointwise = nn.Conv2d(c1, c2, 1, 1, 0, bias=False)
+        self.gn = nn.GroupNorm(32, c2)
+        self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+
+    def forward(self, x):
+
+        x = self.depthwise(x)
+        x = self.pointwise(x)
+        x = self.gn(x)
+        x = self.act(x)
+        return x
+
+
+
+class DyDCNv2(nn.Module):
+
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 stride=1,
+                 norm_cfg=dict(type='GN', num_groups=16, requires_grad=True)):
+        super().__init__()
+        self.with_norm = norm_cfg is not None
+        bias = not self.with_norm
+        self.conv = ModulatedDeformConv2d(
+            in_channels, out_channels, 3, stride=stride, padding=1, bias=bias)
+        if self.with_norm:
+            self.norm = build_norm_layer(norm_cfg, out_channels)[1]
+
+    def forward(self, x, offset, mask):
+
+        x = self.conv(x.contiguous(), offset, mask)
+        if self.with_norm:
+            x = self.norm(x)
+        return x
+
+
+class Scale(nn.Module):
+
+    def __init__(self, scale: float = 1.0):
+        super().__init__()
+        self.scale = nn.Parameter(torch.tensor(scale, dtype=torch.float))
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return x * self.scale
+
+
+class I2P_HBB(nn.Module):
+
+    dynamic = False  # force grid reconstruction
+    export = False  # export mode
+    shape = None
+    anchors = torch.empty(0)  # init
+    strides = torch.empty(0)  # init
+
+    def __init__(self, nc=80, hidc=256, ch=()):
+        super().__init__()
+        self.nc = nc 
+        self.nl = len(ch) 
+        self.reg_max = 16  
+        self.no = nc + self.reg_max * 4 
+        self.stride = torch.zeros(self.nl) 
+
+        self.share_conv = nn.ModuleList([
+             nn.Sequential(Conv_DPGN(hidc // 2, hidc // 2, 3),
+                          Conv_DPGN(hidc, hidc // 2, 3), Conv_DPGN(2 * hidc, hidc // 2, 3)),
+            Conv_GN(hidc // 2, hidc // 2, 3)
+            ])
+        self.cls_decomp = TaskDecomposition(hidc // 2, 2, 16)
+        self.reg_decomp = TaskDecomposition(hidc // 2, 2, 16)
+        self.DyDCNV2 = DyDCNv2(hidc // 2, hidc // 2)
+        self.spatial_conv_offset = nn.Conv2d(hidc, 3 * 3 * 3, 3, padding=1)
+        self.offset_dim = 2 * 3 * 3
+        self.cls_prob_conv1 = nn.Conv2d(hidc, hidc // 4, 1)
+        self.cls_prob_conv2 = nn.Conv2d(hidc // 4, 1, 3, padding=1)
+        self.cv2 = nn.Conv2d(hidc // 2, 4 * self.reg_max, 1)
+        self.cv3 = nn.Conv2d(hidc // 2, self.nc, 1)
+        self.scale = nn.ModuleList(Scale(1.0) for x in ch)
+        self.dfl = DFL(self.reg_max) if self.reg_max > 1 else nn.Identity()
+
+    def forward(self, x):
+        """Concatenates and returns predicted bounding boxes and class probabilities."""
+        for i in range(self.nl):
+            stack_res_list = [self.share_conv[0][i](x[i])]
+            stack_res_list.extend(m(stack_res_list[-1]) for m in self.share_conv[1:])
+            feat = torch.cat(stack_res_list, dim=1)
+            
+            avg_feat = F.adaptive_avg_pool2d(feat, (1, 1))
+            cls_feat = self.cls_decomp(feat, avg_feat)
+            reg_feat = self.reg_decomp(feat, avg_feat)
+
+            
+            offset_and_mask = self.spatial_conv_offset(feat)
+            offset = offset_and_mask[:, :self.offset_dim, :, :]
+            mask = offset_and_mask[:, self.offset_dim:, :, :].sigmoid()
+            reg_feat = self.DyDCNV2(reg_feat, offset, mask)
+            
+
+            cls_prob = self.cls_prob_conv2(F.relu(self.cls_prob_conv1(feat))).sigmoid()
+            
+            x[i] = torch.cat((self.scale[i](self.cv2(reg_feat)), self.cv3(cls_feat * cls_prob)), 1)
+        if self.training:  
+            return x
+
+
+        shape = x[0].shape  
+        x_cat = torch.cat([xi.view(shape[0], self.no, -1) for xi in x], 2)
+        if self.dynamic or self.shape != shape:
+            self.anchors, self.strides = (x.transpose(0, 1) for x in make_anchors(x, self.stride, 0.5))
+            self.shape = shape
+
+        if self.export and self.format in ("saved_model", "pb", "tflite", "edgetpu", "tfjs"):  # avoid TF FlexSplitV ops
+            box = x_cat[:, : self.reg_max * 4]
+            cls = x_cat[:, self.reg_max * 4 :]
+        else:
+            box, cls = x_cat.split((self.reg_max * 4, self.nc), 1)
+        dbox = self.decode_bboxes(box)
+
+        if self.export and self.format in ("tflite", "edgetpu"):
+
+            img_h = shape[2]
+            img_w = shape[3]
+            img_size = torch.tensor([img_w, img_h, img_w, img_h], device=box.device).reshape(1, 4, 1)
+            norm = self.strides / (self.stride[0] * img_size)
+            dbox = dist2bbox(self.dfl(box) * norm, self.anchors.unsqueeze(0) * norm[:, :2], xywh=True, dim=1)
+
+        y = torch.cat((dbox, cls.sigmoid()), 1)
+        return y if self.export else (y, x)
+
+    def bias_init(self):
+        """Initialize Detect() biases, WARNING: requires stride availability."""
+        m = self 
+        m.cv2.bias.data[:] = 1.0 
+        m.cv3.bias.data[: m.nc] = math.log(5 / m.nc / (640 / 16) ** 2) 
+
+    def decode_bboxes(self, bboxes):
+        """Decode bounding boxes."""
+        return dist2bbox(self.dfl(bboxes), self.anchors.unsqueeze(0), xywh=True, dim=1) * self.strides
+
+
+class I2P(I2P_HBB):
 
     def __init__(self, nc=80, ne=1, hidc=256, ch=()):
 
         super().__init__(nc, hidc, ch)
+        self.ne = ne  
+        self.detect = I2P_HBB.forward
+
+        c4 = max(ch[0] // 4, self.ne)
+        self.cv4 = nn.ModuleList(nn.Sequential(Conv_GN(x, c4, 1), Conv_GN(c4, c4, 3), nn.Conv2d(c4, self.ne, 1)) for x in ch)
 
     def forward(self, x):
-
         bs = x[0].shape[0]  # batch size
-        angle = torch.cat([self.cv4[i](x[i]).view(bs, self.ne, -1) for i in range(self.nl)], 2)  # OBB theta logits
-        # NOTE: set `angle` as an attribute so that `decode_bboxes` could use it.
-        angle = (angle.sigmoid() - 0.25) * math.pi  # [-pi/4, 3pi/4]
-        # angle = angle.sigmoid() * math.pi / 2  # [0, pi/2]
+        angle = torch.cat([self.cv4[i](x[i]).view(bs, self.ne, -1) for i in range(self.nl)], 2) 
+        angle = (angle.sigmoid() - 0.25) * math.pi 
         if not self.training:
             self.angle = angle
-        x = Detect.forward(self, x)
+        x = self.detect(self, x)
         if self.training:
             return x, angle
         return torch.cat([x, angle], 1) if self.export else (torch.cat([x[0], angle], 1), (x[1], angle))
 
-    def decode_bboxes(self, bboxes, anchors):
+    def decode_bboxes(self, bboxes):
         """Decode rotated bounding boxes."""
-        return dist2rbox(bboxes, self.angle, anchors, dim=1)
+        return dist2rbox(self.dfl(bboxes), self.angle, self.anchors.unsqueeze(0), dim=1) * self.strides
